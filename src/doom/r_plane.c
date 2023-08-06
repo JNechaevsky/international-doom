4ab678ed676e1eb6c7af49bceef0a1b4c638d1c4
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2023-08-06 13:02:32 +0300

    Update r_plane.c
    r_plane.c:182:19: warning: comparison of integer expressions of different signedness: 'unsigned int' and 'int' [-Wsign-compare]
      182 |         if (index >= MAXLIGHTZ )

--- src/doom/r_plane.c
+++ src/doom/r_plane.c
@@ -126,7 +126,7 @@ R_MapPlane
 //  angle_t	angle;
     fixed_t	distance;
 //  fixed_t	length;
-    unsigned	index;
+//  unsigned	index;
     int dx, dy;
 	
 #ifdef RANGECHECK
@@ -177,12 +177,7 @@ R_MapPlane
 	ds_colormap[0] = ds_colormap[1] = fixedcolormap;
     else
     {
-	index = distance >> LIGHTZSHIFT;
-	
-	if (index >= MAXLIGHTZ )
-	    index = MAXLIGHTZ-1;
-
-        ds_colormap[0] = planezlight[index];
+        ds_colormap[0] = planezlight[BETWEEN(0, distance >> LIGHTZSHIFT, MAXLIGHTZ - 1)];
         ds_colormap[1] = colormaps;
     }
 	
