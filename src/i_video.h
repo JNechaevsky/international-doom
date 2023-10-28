bab69cda5f51b076db037932f7808ead9078b0b8
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2023-10-28 13:01:50 +0300

    Extend MAXWIDTH/HEIGHT arrays (#22)
    * Extend MAXWIDTH/HEIGHT arrays
    
    * Clarify comments

--- src/i_video.h
+++ src/i_video.h
@@ -30,9 +30,9 @@
 #define ORIGWIDTH  320 // [crispy]
 #define ORIGHEIGHT 200 // [crispy]
 
-// [JN] Increase more to support quad rendering resolution.
-#define MAXWIDTH  (ORIGWIDTH << 3)  // [crispy] 
-#define MAXHEIGHT (ORIGHEIGHT << 2) // [crispy] 
+// [JN] Increase more (+2) to support quad rendering resolution.
+#define MAXWIDTH  (ORIGWIDTH << 5)  // [crispy] 
+#define MAXHEIGHT (ORIGHEIGHT << 4) // [crispy] 
 
 extern int SCREENWIDTH;
 extern int SCREENHEIGHT;
@@ -63,7 +63,8 @@ enum
 // Screen height used when vid_aspect_ratio_correct=true.
 
 #define ORIGHEIGHT_4_3 240 // [crispy]
-#define MAXHEIGHT_4_3 (ORIGHEIGHT_4_3 << 1) // [crispy]
+// [JN] Increase more (+2) to support quad rendering resolution.
+#define MAXHEIGHT_4_3 (ORIGHEIGHT_4_3 << 4) // [crispy]
 
 extern int SCREENHEIGHT_4_3;
 
