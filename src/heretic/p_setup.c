4b60f9754c53ec964a47efc27f6f53e318a4b624
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-01-08 21:46:33 +0300

    Check/recreate BLOCKMAP if necessary

--- src/heretic/p_setup.c
+++ src/heretic/p_setup.c
@@ -967,15 +967,20 @@ void P_LoadSideDefs(int lump)
 =================
 */
 
-void P_LoadBlockMap(int lump)
+boolean P_LoadBlockMap(int lump)
 {
     int i, count;
     int lumplen;
     short *wadblockmaplump;
 
-    lumplen = W_LumpLength(lump);
-
-    count = lumplen / 2; // [crispy] remove BLOCKMAP limit
+    // [crispy] (re-)create BLOCKMAP if necessary
+    if (M_CheckParm("-blockmap") ||
+        lump >= numlumps ||
+        (lumplen = W_LumpLength(lump)) < 8 ||
+        (count = lumplen / 2) >= 0x10000)
+    {
+	return false;
+    }
 
     // [crispy] remove BLOCKMAP limit
     wadblockmaplump = Z_Malloc(lumplen, PU_LEVEL, NULL);
@@ -1008,8 +1013,187 @@ void P_LoadBlockMap(int lump)
     count = sizeof(*blocklinks) * bmapwidth * bmapheight;
     blocklinks = Z_Malloc(count, PU_LEVEL, 0);
     memset(blocklinks, 0, count);
+
+    return true;
 }
 
+
+// -----------------------------------------------------------------------------
+// P_CreateBlockMap
+// [crispy] taken from mbfsrc/P_SETUP.C:547-707, slightly adapted
+// -----------------------------------------------------------------------------
+
+static void P_CreateBlockMap (void)
+{
+    int i;
+    fixed_t minx = INT_MAX, miny = INT_MAX, maxx = INT_MIN, maxy = INT_MIN;
+
+    // First find limits of map
+
+    for (i=0; i<numvertexes; i++)
+    {
+        if (vertexes[i].x >> FRACBITS < minx)
+        {
+            minx = vertexes[i].x >> FRACBITS;
+        }
+        else if (vertexes[i].x >> FRACBITS > maxx)
+        {
+            maxx = vertexes[i].x >> FRACBITS;
+        }
+        if (vertexes[i].y >> FRACBITS < miny)
+        {
+            miny = vertexes[i].y >> FRACBITS;
+        }
+        else if (vertexes[i].y >> FRACBITS > maxy)
+        {
+            maxy = vertexes[i].y >> FRACBITS;
+        }
+    }
+
+    // Save blockmap parameters
+
+    bmaporgx = minx << FRACBITS;
+    bmaporgy = miny << FRACBITS;
+    bmapwidth  = ((maxx-minx) >> MAPBTOFRAC) + 1;
+    bmapheight = ((maxy-miny) >> MAPBTOFRAC) + 1;
+
+    // Compute blockmap, which is stored as a 2d array of variable-sized lists.
+    //
+    // Pseudocode:
+    //
+    // For each linedef:
+    //
+    //   Map the starting and ending vertices to blocks.
+    //
+    //   Starting in the starting vertex's block, do:
+    //
+    //     Add linedef to current block's list, dynamically resizing it.
+    //
+    //     If current block is the same as the ending vertex's block, exit loop.
+    //
+    //     Move to an adjacent block by moving towards the ending block in
+    //     either the x or y direction, to the block which contains the linedef.
+
+    {
+        typedef struct { int n, nalloc, *list; } bmap_t;  // blocklist structure
+        unsigned tot = bmapwidth * bmapheight;            // size of blockmap
+        bmap_t *bmap = calloc(sizeof *bmap, tot);         // array of blocklists
+        int x, y, adx, ady, bend;
+
+        for (i=0; i < numlines; i++)
+        {
+            int dx, dy, diff, b;
+
+            // starting coordinates
+            x = (lines[i].v1->x >> FRACBITS) - minx;
+            y = (lines[i].v1->y >> FRACBITS) - miny;
+
+            // x-y deltas
+            adx = lines[i].dx >> FRACBITS, dx = adx < 0 ? -1 : 1;
+            ady = lines[i].dy >> FRACBITS, dy = ady < 0 ? -1 : 1;
+
+            // difference in preferring to move across y (>0) instead of x (<0)
+            diff = !adx ? 1 : !ady ? -1 :
+            (((x >> MAPBTOFRAC) << MAPBTOFRAC) +
+            (dx > 0 ? MAPBLOCKUNITS-1 : 0) - x) * (ady = abs(ady)) * dx -
+            (((y >> MAPBTOFRAC) << MAPBTOFRAC) +
+            (dy > 0 ? MAPBLOCKUNITS-1 : 0) - y) * (adx = abs(adx)) * dy;
+
+            // starting block, and pointer to its blocklist structure
+            b = (y >> MAPBTOFRAC)*bmapwidth + (x >> MAPBTOFRAC);
+
+            // ending block
+            bend = (((lines[i].v2->y >> FRACBITS) - miny) >> MAPBTOFRAC)
+                 * bmapwidth + (((lines[i].v2->x >> FRACBITS) - minx) >> MAPBTOFRAC);
+
+            // delta for pointer when moving across y
+            dy *= bmapwidth;
+
+            // deltas for diff inside the loop
+            adx <<= MAPBTOFRAC;
+            ady <<= MAPBTOFRAC;
+
+            // Now we simply iterate block-by-block until we reach the end block.
+            while ((unsigned) b < tot)    // failsafe -- should ALWAYS be true
+            {
+                // Increase size of allocated list if necessary
+                if (bmap[b].n >= bmap[b].nalloc)
+                    bmap[b].list = I_Realloc(bmap[b].list,
+                   (bmap[b].nalloc = bmap[b].nalloc ?
+                    bmap[b].nalloc*2 : 8)*sizeof*bmap->list);
+
+                // Add linedef to end of list
+                bmap[b].list[bmap[b].n++] = i;
+
+                // If we have reached the last block, exit
+                if (b == bend)
+                {
+                    break;
+                }
+
+                // Move in either the x or y direction to the next block
+                if (diff < 0)
+                {
+                    diff += ady, b += dx;
+                }
+                else
+                {
+                    diff -= adx, b += dy;
+                }
+            }
+        }
+
+        // Compute the total size of the blockmap.
+        //
+        // Compression of empty blocks is performed by reserving two offset words
+        // at tot and tot+1.
+        //
+        // 4 words, unused if this routine is called, are reserved at the start.
+
+        {
+            int count = tot+6;  // we need at least 1 word per block, plus reserved's
+        
+            for (unsigned int t = 0; t < tot; t++)
+                if (bmap[t].n)
+                    count += bmap[t].n + 2; // 1 header word + 1 trailer word + blocklist
+        
+            // Allocate blockmap lump with computed count
+            blockmaplump = Z_Malloc(sizeof(*blockmaplump) * count, PU_LEVEL, 0);
+        }
+
+        // Now compress the blockmap.
+        {
+            int ndx = tot += 4;         // Advance index to start of linedef lists
+            bmap_t *bp = bmap;          // Start of uncompressed blockmap
+
+            blockmaplump[ndx++] = 0;    // Store an empty blockmap list at start
+            blockmaplump[ndx++] = -1;   // (Used for compression)
+
+            for (unsigned int t = 4; t < tot; t++, bp++)
+                if (bp->n)                                      // Non-empty blocklist
+                {
+                    blockmaplump[blockmaplump[t] = ndx++] = 0;  // Store index & header
+                    do
+                    blockmaplump[ndx++] = bp->list[--bp->n];    // Copy linedef list
+                    while (bp->n);
+                    blockmaplump[ndx++] = -1;                   // Store trailer
+                    free(bp->list);                             // Free linedef list
+                }
+                else            // Empty blocklist: point to reserved empty blocklist
+                blockmaplump[t] = tot;
+        
+            free(bmap);    // Free uncompressed blockmap
+        }
+    }
+
+    // [crispy] copied over from P_LoadBlockMap()
+    {
+        int count = sizeof(*blocklinks) * bmapwidth * bmapheight;
+        blocklinks = Z_Malloc(count, PU_LEVEL, 0);
+        memset(blocklinks, 0, count);
+        blockmap = blockmaplump+4;
+    }
+}
 
 
 
@@ -1172,6 +1356,7 @@ void P_SetupLevel(int episode, int map, int playermask, skill_t skill)
     char lumpname[9];
     int lumpnum;
     mobj_t *mobj;
+    boolean	crispy_validblockmap;
     mapformat_t crispy_mapformat;
     // [JN] CRL - indicate level loading time in console.
     const int starttime = SDL_GetTicks();
@@ -1221,12 +1406,18 @@ void P_SetupLevel(int episode, int map, int playermask, skill_t skill)
     maplumpinfo = lumpinfo[lumpnum];
 
 // note: most of this ordering is important     
-    P_LoadBlockMap(lumpnum + ML_BLOCKMAP);
+    crispy_validblockmap = P_LoadBlockMap (lumpnum+ML_BLOCKMAP); // [crispy] (re-)create BLOCKMAP if necessary
     P_LoadVertexes(lumpnum + ML_VERTEXES);
     P_LoadSectors(lumpnum + ML_SECTORS);
     P_LoadSideDefs(lumpnum + ML_SIDEDEFS);
 
     P_LoadLineDefs(lumpnum + ML_LINEDEFS);
+
+    // [crispy] (re-)create BLOCKMAP if necessary
+    if (!crispy_validblockmap)
+    {
+        P_CreateBlockMap();
+    }
 
     if (crispy_mapformat & (MFMT_ZDBSPX | MFMT_ZDBSPZ))
     {
