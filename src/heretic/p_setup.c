c3f8f272907bee87473e163589cce34e549f971f
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-01-08 22:04:41 +0300

    Add missing line

--- src/heretic/p_setup.c
+++ src/heretic/p_setup.c
@@ -2,6 +2,7 @@
 // Copyright(C) 1993-1996 Id Software, Inc.
 // Copyright(C) 1993-2008 Raven Software
 // Copyright(C) 2005-2014 Simon Howard
+// Copyright(C) 2015-2018 Fabian Greffrath
 // Copyright(C) 2016-2024 Julia Nechaevskaya
 //
 // This program is free software; you can redistribute it and/or
