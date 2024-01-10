259abd7723d5eaa1af0275b919b3f3784cbd9d01
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-01-10 12:06:18 +0300

    Heretic: fix for falling down from ledge on save game loading

--- src/heretic/p_saveg.c
+++ src/heretic/p_saveg.c
@@ -1789,8 +1789,10 @@ void P_UnArchiveThinkers(void)
 //              mobj->target = NULL;
                 P_SetThingPosition(mobj);
                 mobj->info = &mobjinfo[mobj->type];
-                mobj->floorz = mobj->subsector->sector->floorheight;
-                mobj->ceilingz = mobj->subsector->sector->ceilingheight;
+                // [JN] killough 2/28/98:
+                // Fix for falling down into a wall after savegame loaded:
+                // mobj->floorz = mobj->subsector->sector->floorheight;
+                // mobj->ceilingz = mobj->subsector->sector->ceilingheight;
                 mobj->thinker.function = P_MobjThinker;
                 P_AddThinker(&mobj->thinker);
                 break;
