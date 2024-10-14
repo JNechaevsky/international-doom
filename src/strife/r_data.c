059f93d1ccd5d9ec3395b09f82e77a57ae35cda7
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-10-14 23:54:39 +0300

    Update r_data.c

--- src/strife/r_data.c
+++ src/strife/r_data.c
@@ -1191,7 +1191,7 @@ void R_InitColormaps (void)
 				b_channel = 
 					(byte) ((0 + a_lo) * playpal[3 * k + 0] +
 							(0 + a_lo) * playpal[3 * k + 1] +
-							(1 - a_hi) * playpal[3 * k + 2] * vid_b_intensity);
+							(1 - a_hi) * playpal[3 * k + 2]) * vid_b_intensity;
 
 				if (i < 224)
 				{
@@ -1276,7 +1276,7 @@ void R_InitColormaps (void)
 		b_channel = 
 			(byte) ((0 + a_lo) * playpal[3 * i + 0] +
 					(0 + a_lo) * playpal[3 * i + 1] +
-					(1 - a_hi) * playpal[3 * i + 2] * vid_b_intensity);
+					(1 - a_hi) * playpal[3 * i + 2]) * vid_b_intensity;
 
 		r = gammatable[vid_gamma][r_channel];
 		g = gammatable[vid_gamma][g_channel];
