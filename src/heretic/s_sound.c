JNechaevsky/international-doom

commit 52861a7b9f70219378fecf722e4ac0a159dcdd47

Heretic, Hexen: fix buffer overflow when playing sounds in some cases (#203)

src/heretic/s_sound.c
@@ -238,7 +238,7 @@ void S_StartSound(void *_origin, int sound_id)
     }
     for (i = 0; i < snd_channels; i++)
     {
-        if (origin->player)
+        if (origin != listener && origin->player)
         {
             i = snd_channels;
             break;              // let the player have more than one sound.

src/hexen/s_sound.c
@@ -251,6 +251,7 @@ void S_StartSound(mobj_t * origin, int sound_id)
     S_StartSoundAtVolume(origin, sound_id, 127);
 }
 
+
 static mobj_t *GetSoundListener(void)
 {
     static degenmobj_t dummy_listener;
@@ -351,7 +352,7 @@ void S_StartSoundAtVolume(mobj_t * origin, int sound_id, int volume)
     #endif
     for (i = 0; i < snd_channels; i++)
     {
-        if (origin->player)
+        if (origin != listener && origin->player)
         {
             i = snd_channels;
             break;              // let the player have more than one sound.

commit 8d407f6f1fcb3e1fedbfe397e24a130fcbf86ba0

Heretic, Hexen: fix some sounds not playing fully
fix regression introduced in PR #203

src/heretic/s_sound.c
@@ -45,6 +45,8 @@ static boolean S_StopSoundID(int sound_id, int priority);
 
 static channel_t channel[MAX_CHANNELS];
 
+static degenmobj_t dummy_listener;
+
 static void *rs;          // Handle for the registered song
 int mus_song = -1;
 int mus_lumpnum;
@@ -149,8 +151,6 @@ void S_StartSong(int song, boolean loop)
 
 static mobj_t *GetSoundListener(void)
 {
-    static degenmobj_t dummy_listener;
-
     // If we are at the title screen, the console player doesn't have an
     // object yet, so return a pointer to a static dummy listener instead.
 
@@ -238,7 +238,7 @@ void S_StartSound(void *_origin, int sound_id)
     }
     for (i = 0; i < snd_channels; i++)
     {
-        if (origin != listener && origin->player)
+        if (origin != (mobj_t *)&dummy_listener && origin->player)
         {
             i = snd_channels;
             break;              // let the player have more than one sound.

src/hexen/s_sound.c
@@ -67,6 +67,8 @@ int snd_MusicVolume = 10;              // maximum volume for music
 static int sfxVolume;
 static int musVolume;
 
+static degenmobj_t dummy_listener;
+
 // [JN] Enforce music replay while changing music system.
 boolean mus_force_replay = false;
 
@@ -251,11 +253,8 @@ void S_StartSound(mobj_t * origin, int sound_id)
     S_StartSoundAtVolume(origin, sound_id, 127);
 }
 
-
 static mobj_t *GetSoundListener(void)
 {
-    static degenmobj_t dummy_listener;
-
     // If we are at the title screen, the console player doesn't have an
     // object yet, so return a pointer to a static dummy listener instead.
 
@@ -352,7 +351,7 @@ void S_StartSoundAtVolume(mobj_t * origin, int sound_id, int volume)
     #endif
     for (i = 0; i < snd_channels; i++)
     {
-        if (origin != listener && origin->player)
+        if (origin != (mobj_t *)&dummy_listener && origin->player)
         {
             i = snd_channels;
             break;              // let the player have more than one sound.