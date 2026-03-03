422a8c614cc7b318db51a312f1dd78f17ae3c076
Julia Nechaevskaya <pnvictress@gmail.com>
2026-03-03 18:55:18 +0300

    ASAN: Keep angle in finetangent array

--- src/doom/r_segs.c
+++ src/doom/r_segs.c
@@ -352,7 +352,8 @@ static void R_RenderSegLoop (void)
         if (segtextured)
         {
             // calculate texture offset
-            const angle_t angle = (rw_centerangle + xtoviewangle[rw_x]) >> ANGLETOFINESHIFT;
+            const angle_t angle = ((rw_centerangle + xtoviewangle[rw_x]) >> ANGLETOFINESHIFT)
+                                & FINEANGLES_TAN; // [PN] ASAN: Keep angle in finetangent array
             texturecolumn = rw_offset - FixedMul(finetangent[angle], rw_distance);
             texturecolumn >>= FRACBITS;
 
