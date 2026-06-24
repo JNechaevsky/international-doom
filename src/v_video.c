75b5bab7c7b683c5aca8a3bfa513cc41769e7331
Julia Nechaevskaya <pnvictress@gmail.com>
2026-06-24 19:47:35 +0300

    Shut up stb_image_write warnings

--- src/v_video.c
+++ src/v_video.c
@@ -25,6 +25,9 @@
 #define MINIZ_NO_ZLIB_APIS
 #include "miniz.h"
 
+// [PN] Provide prototypes for internal stb_image_write functions to silence -Wmissing-prototypes
+static unsigned char *stbi_zlib_compress(unsigned char *data, int data_len, int *out_len, int quality);
+static unsigned char *stbi_write_png_to_mem(const unsigned char *pixels, int stride_bytes, int x, int y, int n, int *out_len);
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
